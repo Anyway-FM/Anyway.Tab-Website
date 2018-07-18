ea9844eb5446f50f49518009ad77f57fff4c3dae
JJ Ying <yingjunjiu@gmail.com>
2018-07-18 17:19:39 +0800

    去掉了 inline 安装模式

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
 	gulpLoadPlugins = require('gulp-load-plugins'),
-    plugins = gulpLoadPlugins();
- 
+  plugins = gulpLoadPlugins();
+
 var cdnUrl = [
 	[ '<link rel=\"stylesheet\" rev=\"stylesheet\" href=\"assets/fonts.css\" type=\"text/css\" media=\"all\" />', '' ],
 	[ 'assets/in-view.min.js', 'https://anyway-web.b0.upaiyun.com/js/in-view.min.js' ],
@@ -12,7 +12,6 @@ var fontUrl = [
 	[ 'fonts/', 'https://anyway-web.b0.upaiyun.com/anyway.tab/' ]
 ];
 
- 
 gulp.task('default', function() {
 	gulp.src(['index.html','*.php'])
 		.pipe(plugins.fontSpider({ignore: ['assets/fonts.css']}));
@@ -20,19 +19,19 @@ gulp.task('default', function() {
 	gulp.src(['*.html','*.php'])
 		.pipe(plugins.batchReplace(cdnUrl))
 		.pipe(gulp.dest('builds'));
-		
+
 	gulp.src('assets/fonts/*.*')
 	        .pipe(gulp.dest('builds'));
-	        
+
 	gulp.src('*.svg')
 	        .pipe(plugins.svgo())
 	        .pipe(gulp.dest('builds'));
-	
+
 	gulp.src(['assets/*.css','!assets/*.min.css'])
 		.pipe(plugins.concat('main.css'))
 		.pipe(plugins.batchReplace(fontUrl))
 		.pipe(plugins.cleanCss({compatibility: 'ie8'}))
-		.pipe(gulp.dest('builds'));     
+		.pipe(gulp.dest('builds'));
 });
 
 gulp.task('watch', function() {
